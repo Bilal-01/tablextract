333600685d2bcee7c6fb798e27515e0a8b2ad001
Bilal-01 <mbilalaziz.01@gmail.com>
2023-04-10 08:20:42 +0500

    creating directory structure

---
+++ src/api/authservice.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Handling APIs</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
