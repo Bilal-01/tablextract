81f313aebd474684751fdcf2b2a1c4786402b146
Bilal-01 <mbilalaziz.01@gmail.com>
2023-04-10 14:45:32 +0500

    firebase setup and initialization

---
+++ src/components/atoms/button.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import {
+    StyleSheet,
+    Button,
+    View,
+    Text,
+    Alert,
+  } from 'react-native';
+
+
+export default function Button(props) {
+  return (
+    <View style={styles.container}>
+      <Button
+        title = {props.title}
+        onPress = {props.onPressHandler}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    
+});
