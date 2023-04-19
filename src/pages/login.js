4f7526c6915b680e19961730cb8f9e826231abaa
= <=>
2023-04-20 03:20:41 +0500

    signup and login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { StyleSheet,View, Text, TextInput, TouchableOpacity } from 'react-native';
-import firebase from '../../firebaseConfig';
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { Image,StyleSheet,View, Text, TextInput, TouchableOpacity } from 'react-native';
+// import app from '../../firebaseConfig';
+import { sendPasswordResetEmail,getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import logo from '../assets/logo.png';
 
 
 const Login = ({ navigation }) => {
@@ -18,33 +19,63 @@ const Login = ({ navigation }) => {
         alert('Error logging in. Email or password invalid');
       });
   }
+  const handleForgotPassword = () => {
+    const auth = getAuth();
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        alert('Password reset email sent. Please check your inbox.');
+      })
+      .catch((error) => {
+        if (error.code === 'auth/user-not-found') {
+          alert('Error: Email not registered. Please try again.');
+        } else {
+          alert('Enter your email address.');
+        }
+      });
+  }
+  
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Login</Text>
+    <View style={{ color:'white',backgroundColor:'black',flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {/* <Text style={styles.loginText}>Login</Text> */}
+      <Image source={logo} style={styles.image} />
+      <View style={styles.inputView}>
       <TextInput
-        style={{ marginTop: 20, padding: 10, borderWidth: 1 }}
+        style={{ ...styles.TextInput}}
         placeholder="Email"
+        // placeholderTextColor='#003f5c'
         onChangeText={(inputEmail) => setEmail(inputEmail)}
-      />
+        />
+      </View>
+      <View style={styles.inputView}>
       <TextInput
-        style={{ marginTop: 20, padding: 10, borderWidth: 1 }}
+        style={{ ...styles.TextInput}}
         placeholder="Password"
         secureTextEntry={true}
         onChangeText={(inputPass) => setPassword(inputPass)}
       />
-      <TouchableOpacity
-        onPress={handleLogin}
-      >
-        <Text style={{ color: colors.text, ...styles.buttonText,...styles.loginButton }}>Login</Text>
+      </View>
+      <TouchableOpacity style={styles.loginBtn}onPress={handleLogin}>
+        <Text style={styles.loginText}>LOGIN</Text> 
       </TouchableOpacity>
       <TouchableOpacity
-        style={{ marginTop: 20, padding: 10 }}
+        onPress={handleForgotPassword}
+      >
+        <Text style={{...styles.forgot_button,color:'white'}}>Forgot Password?</Text> 
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{ marginTop: 0, padding: 10 }}
         onPress={() => navigation.navigate('Signup')}
       >
-        <Text>Don't have an account? Sign up</Text>
+        <Text style={{color:'white'}}>Don't have an account? Sign up</Text>
       </TouchableOpacity>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>Contact Us</Text>
+        <Text style={styles.footerText}>tablextract.app@gmail.com</Text>
+      </View>
     </View>
+    
   );
 };
 const colors = {
@@ -56,27 +87,68 @@ const colors = {
   text: '#253e53'
 };
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-    color: colors.text
+  loginBtn:
+  {
+    width:"45%",
+    borderRadius:5,
+    height:40,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:20,
+    marginBottom:10,
+    backgroundColor:colors.secondary,
   },
-  buttonText: {
-    color: colors.text,
-    fontSize: 14,
-    fontWeight: 'bold',
-    textAlign: 'center',
+  loginText:{
+    fontSize:20,
+    color : colors.text,
+    fontWeight:'bold'
   },
-  loginButton: {
-    backgroundColor: colors.secondary,
-    paddingVertical: 10,
-    paddingHorizontal: 15,
+  inputView: {
+    backgroundColor: colors.primary,
     borderRadius: 30,
-    marginBottom: 5,
-    marginTop: 20,
-    width: 200,
-    alignSelf:'center'
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center",
   },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    // marginLeft: 20,
+    color: colors.text,
+  },
+   forgot_button: {
+    height: 30,
+    marginTop:20,
+    marginBottom: 0,
+  },
+  image: {
+    marginBottom: 40,
+    width: 100,
+    height: 100,
+    alignItems: 'center',
+    marginLeft:10,
+    borderRadius: 20,
+  },  
+  footer: {
+    backgroundColor: colors.base_f,
+    padding: 10,
+    alignItems: 'center',
+    marginTop: 5,
+    width:'100%',
+    bottom:0,
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+  },
+  footerText: {
+    fontSize: 16,
+    lineHeight: 24,
+    textAlign: 'center',
+    marginBottom: 5,
+    color: colors.gray,
+  }
 })
 
 export default Login;
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet,View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 
@@ -55,48 +55,136 @@ const Signup = ({ navigation }) => {
   };
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Sign Up Screen</Text>
+  
+    <View style={{ color:'white',backgroundColor:'black',flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {/* <Text>Sign Up</Text> */}
+      
+      <View style={{marginTop:60,...styles.inputView}}>
       <TextInput
-        style={{ marginTop: 20, padding: 10, borderWidth: 1 }}
+        style={{ ...styles.TextInput}}
         placeholder="Email"
         onChangeText={(inputEmail) => setEmail(inputEmail)}
       />
+      </View>
+      <View style={styles.inputView}>
       <TextInput
-        style={{ marginTop: 20, padding: 10, borderWidth: 1 }}
+        style={{ ...styles.TextInput}}
         placeholder="Password"
         secureTextEntry={true}
         onChangeText={(inputPass) => setPassword(inputPass)}
       />
+      </View>
+      <View style={styles.inputView}>
       <TextInput
-        style={{ marginTop: 20, padding: 10, borderWidth: 1 }}
+        style={{ ...styles.TextInput}}
         placeholder="Confirm Password"
         secureTextEntry={true}
         onChangeText={(inputConfirmPass) => setConfirmPassword(inputConfirmPass)}
       />
+      </View>
        <TouchableOpacity
-        style={{ marginTop: 20, padding: 10, backgroundColor: 'blue' }}
+        style={styles.signUpBtn}
         onPress={handleSignUp}
         // onPress={() => navigation.navigate('Home')}
 
       >
-        <Text style={{ color: 'white' }}>Sign Up</Text>
+        <Text style={{ color: 'white' ,...styles.signUpText}}>Sign Up</Text>
       
       </TouchableOpacity>
       <TouchableOpacity
         style={{ marginTop: 20, padding: 10 }}
         onPress={() => navigation.navigate('Login')}
       >
-        <Text>Already have an account? Login</Text>
+        <Text style={{color:'white'}}>Already have an account? Login</Text>
       </TouchableOpacity>
       <TouchableOpacity
-        style={{ marginTop: 20, padding: 10 }}
+        style={{ marginTop: 0,marginBottom:20}}
         onPress={() => navigation.navigate('Home')}
       >
-        <Text>Go to Home Page</Text>
+       <Text style={{color:'white'}}>Go to Home Page</Text>
       </TouchableOpacity>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>Contact Us</Text>
+        <Text style={styles.footerText}>tablextract.app@gmail.com</Text>
+      </View>
     </View>
   );
 };
+const colors = {
+  primary: '#F3E8FF',
+  secondary: '#CE5959',
+  base: '#BACDDB',
+  gray: '#808080',
+  base_f: '#454545',
+  text: '#253e53'
+};
+const styles = StyleSheet.create({
+  signUpBtn:
+  {
+    width:"45%",
+    borderRadius:5,
+    height:40,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:20,
+    marginBottom:10,
+    backgroundColor:colors.secondary,
+  },
+  signUpText:{
+    fontSize:18,
+    color : colors.text,
+    fontWeight:'bold'
+  },
+  inputView: {
+    backgroundColor: colors.primary,
+    borderRadius: 30,
+    width: "70%",
+    height: 50,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    // marginLeft: 20,
+    color: colors.text,
+  },
+   forgot_button: {
+    height: 30,
+    marginTop:20,
+    marginBottom: 0,
+  },
+  image: {
+    marginBottom: 40,
+    width: 100,
+    height: 100,
+    alignItems: 'center',
+    marginLeft:10,
+    borderRadius: 20,
+    marginTop: 60,
+  },  
+  footer: {
+    backgroundColor: colors.base_f,
+    padding: 10,
+    alignItems: 'center',
+    marginTop: 5,
+    width:'100%',
+    bottom:0,
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+  },
+
+  footerText: {
+    fontSize: 16,
+    lineHeight: 24,
+    textAlign: 'center',
+    marginBottom: 5,
+    color: colors.gray,
+  }
+})
+
 
 export default Signup;
