1b69f7f9b32b0cce95a54e5e2b1a213a8b7bef63
Bilal-01 <mbilalaziz.01@gmail.com>
2023-04-25 12:10:38 +0500

    image uploader added download logic handled

--- src/pages/uploadfile.js
+++ src/pages/uploadfile.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, Animated, Easing, StyleSheet } from 'react-native';
+import { View, Image, Text, Button, Animated, Easing, StyleSheet } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 
 export default function UploadFile( { navigation } ){
     const [loading, setLoading] = useState(false);
     const [showDownloadButton, setShowDownloadButton] = useState(false);
     const [translateAnim] = useState(new Animated.ValueXY({ x: 0, y: 0 }));
+    const [image, setImage] = useState('');
 
     useEffect(() => {
         if (loading) {
@@ -52,10 +55,23 @@ export default function UploadFile( { navigation } ){
         translateAnim.setValue({ x: 0, y: 0 });
     };
 
-    const handleUpload = () => {
+    const handleUpload = async () => {
         setLoading(true);
 
-        // TODO: Implement document upload logic here
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+        console.log(result.assets[0].uri);
+    
+        if (!result.canceled) {
+            setImage(result.assets[0].uri);
+            if(image){
+                console.log("image set")
+            }
+        }
 
         setTimeout(() => {
         setLoading(false);
@@ -64,32 +80,47 @@ export default function UploadFile( { navigation } ){
     };
 
     const handleDownload = () => {
-        // TODO: Implement download logic here
+        const fileUri = 'F:/bilal/mobile/scan2csv/src/assets/diabetes.csv';
+        const fileUriParts = fileUri.split('/');
+        const fileName = fileUriParts[fileUriParts.length - 1];
+        const downloadResumable = FileSystem.createDownloadResumable(
+            fileUri,
+            FileSystem.documentDirectory + fileName
+        );
+
+        try {
+            const { uri } = downloadResumable.downloadAsync();
+            console.log('Downloaded file:', uri);
+        } catch (error) {
+            console.error('Error while downloading the file:', error);
+        }
     };
 
     return (
         <View style={{ color:'white',backgroundColor:'black',flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Animated.View style={{ transform: translateAnim.getTranslateTransform() }}>
-            <Icon name="search" size={50} color="#BACDDB" />
-        </Animated.View>
-        <Text style={{ marginTop: 20,color:'white'}}>
-            {loading ? 'Extracting table and converting to CSV' : ''}
-        </Text>
-        {showDownloadButton ? (
+            {image && <Image source={{ uri: image }} style={styles.image} />}
+            <Animated.View style={{ transform: translateAnim.getTranslateTransform() }}>
+                <Icon name="search" size={50} color="#BACDDB" />
+            </Animated.View>
+            <Text style={{ marginTop: 20,color:'white'}}>
+                {loading ? 'Extracting table and converting to CSV' : ''}
+            </Text>
+            {showDownloadButton ? (
             <TouchableOpacity style={styles.downloadBtn} onPress={handleDownload}>
-            <Text style={styles.downloadText}>Download CSV</Text>
-          </TouchableOpacity>          
-        ) : (
-            <TouchableOpacity style={styles.uploadBtn} onPress={handleUpload}>
-            <Text style={styles.uploadText}>Upload Image</Text>
-            <Text>File Format: jpeg, jpg, and png </Text>
-             </TouchableOpacity>
-            // <Button   style={styles.uploadBtn} title="Upload Document" onPress={handleUpload} />
-        )}
-        <View style={styles.footer}>
-        <Text style={styles.footerText}>Contact Us</Text>
-        <Text style={styles.footerText}>tablextract.app@gmail.com</Text>
-      </View>
+                <Text style={styles.downloadText}>Download CSV</Text>
+            </TouchableOpacity>          
+            ) : (
+                <>
+                    <TouchableOpacity style={styles.uploadBtn} onPress={handleUpload}>
+                        <Text style={styles.uploadText}>Upload Image</Text>
+                    </TouchableOpacity>
+                    <Text style={{color:'white'}} >File Format: jpeg, jpg, and png </Text>
+                </>
+            )}
+            <View style={styles.footer}>
+                <Text style={styles.footerText}>Contact Us</Text>
+                <Text style={styles.footerText}>tablextract.app@gmail.com</Text>
+            </View>
         </View>
     );
 }
@@ -110,8 +141,7 @@ const styles = StyleSheet.create({
     uploadBtn:
     {
       borderRadius:5,
-      height:50,
-      paddingVertical: 10,
+      paddingVertical: 20,
       paddingHorizontal: 20,
       alignItems:"center",
       justifyContent:"center",
